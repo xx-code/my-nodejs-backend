17706046c1982487f8236fe1252aec2a0e252a64
unknown <akaaugustejunior@gmail.com>
2022-05-22 01:58:17 -0400

    add project

---
+++ src/models/Project.ts
@@ -0,0 +1,82 @@
+import mongoose, { model, Model, Document } from "mongoose";
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: String,
+        required: true,
+        default: 'Me'
+    },
+    developers: [
+        {
+            userId: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            isRegister: {
+                type: Boolean,
+                required: false
+            },
+            name: String
+        }
+    ],
+    links: [
+        {
+            url: {
+                type: String,
+                required: true
+            },
+            plateform: {
+                type: String,
+                required: true,
+                enum: ['Android', 'IOS', 'WINDOWS', 'MACOS']
+            }
+        }
+    ]
+});
+
+export interface developer extends Document {
+    userId?: string,
+    isRegister: boolean,
+    name: string
+};
+
+export interface link extends Document {
+    url: string,
+    plateform: string
+};
+
+export interface project extends Document{
+    name: string,
+    description: string,
+    owner: string,
+    developers: Array<developer>,
+    links: Array<link> 
+};
+
+export interface projectError {
+    nameError?: string,
+    descriptionError?: string,
+    ownerError?: string
+};
+
+export interface developerError {
+    isRegister?: string
+};
+
+export interface linkError{
+    urlError?: string,
+    plateform?: string
+};
+
+const Project: Model<project> = model('projects', ProjectSchema);
+
+export default Project;
