da11b78bf8e2a954332ca8a7936065c1fce88af4
unknown <akaaugustejunior@gmail.com>
2022-05-22 00:40:30 -0400

    add model Resume

---
+++ src/models/Resume.ts
@@ -0,0 +1,66 @@
+import mongoose, { model, Model, Document } from 'mongoose';
+const Schema = mongoose.Schema
+
+const ResumeSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'users'
+    },
+    aboutYou: String,
+    experiences: [
+        {
+            title: String,
+            startDate: {
+                type: Date,
+                required: true
+            },
+            endDate: Date,
+            isCurrent: {
+                type: Boolean,
+                required: true
+            },
+            typeExperience: {
+                type: String,
+                enum: ['Work', 'Certification', 'Education'],
+                default: 'Work'
+            }
+        }
+    ]
+});
+
+export enum TypeExperience {
+    Work='Work',
+    Certification='Certification',
+    Education='Education'
+};
+
+export interface experience extends Document {
+    title: string,
+    startDate: Date,
+    endDate?: Date,
+    isCurrent: boolean,
+    typeExperience: string
+};
+
+export interface resume extends Document {
+    user: string,
+    aboutYou: string,
+    experiences: Array<experience>
+};
+
+export interface resumeError {
+    user?: string,
+    about?: string
+};
+
+export interface experienceError {
+    titleError: string,
+    startDateError: string,
+    typeExperience: string,
+    isCurrent: string
+};
+
+const Resume: Model<resume> = model('resumes', ResumeSchema);
+
+export default Resume;
