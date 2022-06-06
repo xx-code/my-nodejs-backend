c11bb9a04436a2d2a7cf70773b7db0a6b59120e9
unknown <akaaugustejunior@gmail.com>
2022-06-06 12:37:05 -0400

    add model Developper

---
+++ src/models/Developper.ts
@@ -0,0 +1,40 @@
+import mongoose, { model, Model, Document } from "mongoose";
+import { currentUser } from "./User";
+const Schema = mongoose.Schema;
+
+const DevelopperSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    isRegister: {
+        type: Boolean,
+        default: false
+    },
+    name: String,
+    creationDate: {
+        type: Date,
+        default: Date.now()
+    },
+    updateDate: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+export interface developper {
+    user?: string|currentUser,
+    name?: string,
+    isRegister: boolean,
+    creationDate: Date,
+    updateDate: Date
+};
+
+export interface developperError {
+    userError?: string,
+    nameError?: string,
+}
+
+const Developper: Model<developper> = model('developpers', DevelopperSchema);
+
+export default Developper;
