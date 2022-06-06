97496460aae43c52a227f00f0207b4825fcb4f3e
unknown <akaaugustejunior@gmail.com>
2022-06-06 14:03:06 -0400

    add projectlink to collect all link of ours projects

---
+++ src/models/ProjectLink.ts
@@ -0,0 +1,34 @@
+import mongoose, { model, Model, Document } from "mongoose";
+import { project } from "./Project";
+const Schema = mongoose.Schema;
+
+const ProjectLinkSchema = new Schema({
+    project: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'projects'
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    plateform: {
+        type: String,
+        required: true
+    }
+});
+
+export interface projectLink extends Document{
+    project: string|project,
+    url: string,
+    plateform: string
+};
+
+export interface projectLinkError {
+    urlError?: string,
+    plateform?: string
+};
+
+const ProjectLink: Model<projectLink> = model('projectLinks', ProjectLinkSchema);
+
+export default ProjectLink;
