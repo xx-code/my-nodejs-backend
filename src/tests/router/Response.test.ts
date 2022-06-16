28d1b8be9053540b6aeea42935bc78e3a162bb8e
unknown <akaaugustejunior@gmail.com>
2022-06-15 23:30:04 -0400

    corrige matchLanguage fonction for test

--- src/tests/router/Response.test.ts
+++ src/tests/router/Response.test.ts
@@ -5,13 +5,13 @@ const res = { status: jest.fn((status: number) => ({ send: jest.fn((object: any)
 test('test error response', () => {    
     const resultExpected = {error: 'error', message: 'message d\'erreur'};
     Response.instance = res;
-    const result = Response.pushError(ResponseCode.Bad_Request, 'error', 'message d\'erreur');
+    const result = Response.pushError(ResponseCode.Bad_Request.code, 'error', 'message d\'erreur');
     expect(resultExpected).toEqual(result);
 });
 
 test('test success response', () => {
     const resultExpected = {success: true};
     Response.instance = res;
-    const result = Response.pushSuccess(ResponseCode.OK, {success: true});
+    const result = Response.pushSuccess(ResponseCode.OK.code, {success: true});
     expect(resultExpected).toEqual(result);
 });
