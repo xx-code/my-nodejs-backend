24f1969429c461508f721ad60c8fb2265c214fb3
unknown <akaaugustejunior@gmail.com>
2022-07-04 00:53:54 -0400

    modify resumeRouter

--- src/router/user/Resume.ts
+++ src/router/user/Resume.ts
@@ -1,4 +1,4 @@
-import Resume, { resumeError } from "../../models/Resume";
+import Resume, { resumeError, TypeExperience } from "../../models/Resume";
 import User from "../../models/User";
 import Request from '../Request';
 import { request, ResponseCode } from "../Response";
@@ -11,7 +11,7 @@ const userErrorMessage = require('./errors/errorsUser.json')
 
 export default class ResumeRequest implements Request{
     async add(req: request) {
-        const lang = req.lang;
+        const lang = Utils.matchLanguage(req);;
         const input = req.body;
         const { isValid, errors } = this.inputValidation(input, lang);
 
@@ -25,9 +25,17 @@ export default class ResumeRequest implements Request{
         } catch (err) {
             throw { code: ResponseCode.Bad_Request.code, error: err };
         }
+
+        // yyyy-mm-dd
+        input.startDate = new Date(input.startDate);
+        if (!Utils.isEmpty(input.endDate))
+            input.endDate = new Date(input.endDate);
+
+        const newResume = new Resume(input);
+        await newResume.save();
     }
     async update(req: request) {
-        const lang = req.lang;
+        const lang = Utils.matchLanguage(req);;
         const params = req.params;
         const id = params.id;
         const input = req.body;
@@ -46,7 +54,7 @@ export default class ResumeRequest implements Request{
         await resume.updateOne({$set: input});
     }
     async delete(req: request) {
-        const lang = req.lang;
+        const lang = Utils.matchLanguage(req);
         const params = req.params;
         const id = params.id;
 
@@ -68,7 +76,7 @@ export default class ResumeRequest implements Request{
         return resumes;
     }
     async read(req: request) {
-        const lang = req.lang;
+        const lang = Utils.matchLanguage(req);;
         const params = req.params;
         const id = params.id;
         
@@ -91,7 +99,11 @@ export default class ResumeRequest implements Request{
 
         input.user = !Utils.isEmpty(input.user) ? input.user : '';
         input.title = !Utils.isEmpty(input.title) ? input.title : '';
+        input.detail = !Utils.isEmpty(input.detail) ? input.detail : '';
+        input.type = !Utils.isEmpty(input.type) ? input.type: '';
         input.isCurrent = !Utils.isEmpty(input.isCurrent) ? input.isCurrent : false;
+        input.startDate = !Utils.isEmpty(input.startDate) ? input.startDate : '';
+        input.endDate = !Utils.isEmpty(input.endDate) ? input.endDate : '';
 
         if (Validator.isEmpty(input.user)) {
             errors.userError = errorMessage.haveToTargetUser[language];
@@ -102,6 +114,22 @@ export default class ResumeRequest implements Request{
             errors.titleError = errorMessage.fieldTitleError[language];
             isValid = false;
         }
+
+        if (Validator.isEmpty(input.detail)) {
+            errors.detailError = errorMessage.fieldDetail[language];
+            isValid = false;
+        }
+
+        if ( input.type != TypeExperience.Certification && input.type != TypeExperience.Education && input.type != TypeExperience.Work ) {
+            errors.typeError = errorMessage.typeNotValid[language];
+            isValid = false;
+        }
+
+        if (Validator.isEmpty(input.type)) {
+            errors.typeError = errorMessage.fieldTypeError[language];
+            isValid = false;
+        }
+
 
         if (Validator.isEmpty(input.startDate)) {
             errors.startDateError = errorMessage.fieldStartDateError[language];
@@ -123,7 +151,21 @@ export default class ResumeRequest implements Request{
                     isValid = false;
                 }
             }
-        } 
+        } else {
+            const startDate = new Date(input.startDate);
+            const start = moment(startDate.toISOString());
+            
+            if (start > moment()) { 
+                errors.endDateError = errorMessage.CantStartBefort[language];
+                isValid = false;
+            }
+
+            if (!Validator.isEmpty(input.endDate)) {
+                errors.endDateError = errorMessage.EndDateMustNotDefined[language];
+                isValid = false;
+            }
+        }
+            
 
         return { isValid, errors};
     }
