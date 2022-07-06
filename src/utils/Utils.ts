0285a9e9179edc5a37818603d09a6be1860b129f
unknown <akaaugustejunior@gmail.com>
2022-07-05 22:27:34 -0400

    add multer to upload file

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,4 +1,5 @@
 import { request } from "../router/Response";
+import multer from 'multer';
 
 export namespace Utils {
     export function isEmpty(value: any) {
@@ -20,5 +21,34 @@ export namespace Utils {
     export function matchLanguage(req: request) {
         const query = !isEmpty(req.query) ? req.query : '';
         return !isEmpty(query.lang) ? query.lang.toString() : 'en'
+    }
+
+    export function getUploader() {
+        const storage = multer.diskStorage({
+            destination: (req, file, cb) => {
+                cb(null, './temp/');
+            },
+            filename: (req, file, cb) => {
+                cb(null, 'temp' + file.originalname);
+            }
+        });
+
+        const filter = (req:any, file:any, cb: any) => {
+            if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+                cb(null, true);
+            } else {
+                cb(null, false);
+            }
+        };
+
+        const uploader = multer({
+            storage: storage,
+            fileFilter: filter,
+            limits: {
+                fileSize: 1024 * 1024 * 5
+            }
+        });
+
+        return uploader;
     }
 }
